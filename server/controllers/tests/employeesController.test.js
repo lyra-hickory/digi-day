fc985b2ca27bbc8feaab94dbe661a939784a73db
Abigail Lucas <abigaillucas@protonmail.com>
2025-11-12 10:15:32 +0200

    WIP: employeesController tests

---
+++ server/controllers/tests/employeesController.test.js
@@ -0,0 +1,23 @@
+const { describe, it } = require('node:test');
+
+describe('employeesController', () => {
+    const { employeesController } = require('../employeesController');
+
+    describe('getAllEmployees', () => {
+        it('should return a list of all the employees', () => {
+
+        });
+    });
+    describe('getEmployee', () => {
+
+    });
+    describe('createEmployee', () => {
+
+    });
+    describe('updateEmployee', () => {
+
+    });
+    describe('deleteEmployee', () => {
+
+    });
+});
