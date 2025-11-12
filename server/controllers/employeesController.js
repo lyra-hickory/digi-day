91743fced7c54eb543918c94a01c772c35b2adbf
Abigail Lucas <abigaillucas@protonmail.com>
2025-11-12 08:26:41 +0200

    Implement employeesController
    - add data logic to API

--- server/controllers/employeesController.js
+++ server/controllers/employeesController.js
@@ -1,32 +1,60 @@
 
-const path = require('path');
-const data = {};
-data.employees = require(path.join(__dirname, '..', 'model', 'employees.json'));
+const data = {
+    employees : require('../model/employees.json'),
+    setEmployees : (data) => { this.employees = data },
+};
 
 const getAllEmployees = (req, res) => {
     res.json(data.employees);
 };
 
 const getEmployee = (req, res) => {
-    res.json({"id": req.params.id});
+    if(!req.body.id) {
+        return res.status(400).json({ 'message' : 'id field are required.'});
+    }
+    const employee = data.employees.find((emp) => emp.id === parseInt(req.params.id));
+    if(!employee) return res.status(400).json({ 'message' : `Employee ID: "${req.body.id}" not found.`});
+    return res.json(employee);
 };
 
 const createNewEmployee = (req, res) => {
-    res.json({
-        "firstname": req.body.firstname,
-        "lastname": req.body.lastname,
-    });
+    if(!req.body.firstname || !req.body.lastname) {
+        return res.status(400).json({ 'message' : 'firstname, lastname fields are required.'});
+    }
+
+    const newEmployee = {
+        id: data.employees[data.employees.length - 1].id +1 || 1,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+    };
+
+    data.setEmployees([...data.employees, newEmployee]);
+    res.status(201).json(data.employees);
 };
 
 const updateEmployee = (req, res) => {
-    res.json({
-        "firstname": req.body.firstname,
-        "lastname": req.body.lastname,
-    });
+    if(!req.body.id || (!req.body.firstname && !req.body.lastname)) {
+        return res.status(400).json({ 'message' : 'id and first- or lastname fields are required.'});
+    }
+    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+    if(!employee) {
+        return res.status(400).json({ 'message' : `Employee ID ${req.body.id} not found.`});
+    }
+    if(req.body.firstname) employee.firstname = req.body.firstname;
+    if(req.body.lastname) employee.lastname = req.body.lastname;
+    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+    const unsortedArray = [...filteredArray, employee];
+    data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
+    res.json(data.employees);
 };
 
 const deleteEmployee = (req, res) => {
-    res.json({"id": req.body.id});
+    if(!req.body.id) {
+        return res.status(400).json({ 'message' : 'id field are required.'});
+    }
+    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+    data.setEmployees(filteredArray);
+    res.json(data.employees);
 };
 
 module.exports = {
